f72441e7ea4e6b74de17aa84d3ffdafad2862bed
Xynnix <zkjcksn@gmail.com>
2020-05-03 05:03:42 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Auto Bot
+### Automated Basic bot setup script for discord.js
+
+All you need to do is:
+
+- run `bash run.sh`
+- Then insert your bot token in the config.json file
+- run `node .`
+
+> if done correctly.you will see your bot online and you can send `ping` command with your prefix in the beginning of the message!
+
+• Editing the index.js manually is allowed since how else would you make it do more?
---
+++ run.sh
@@ -0,0 +1,24 @@
+mkdir Bot && cd Bot
+touch index.js && touch config.json
+echo "const Discord = require('discord.js');" >> index.js
+echo "const client = new Discord.Client()" >> index.js
+echo "const config = require('./config.json');" >> index.js
+echo "  " >> index.js
+echo "client.on('ready', () =>{" >> index.js
+echo "    console.log('Ready!');" >> index.js
+echo "};" >> index.js
+echo "client.on('message', (message) =>{" >>index.js
+echo "    if (message.author.bot) return;" >> index.js
+echo "    if (message.content === prefix + 'ping')" >> index.js
+echo "       message.channel.send('pang');" >> index.js
+echo "client.login(config.token);" >> index.js
+echo "{" >> config.json
+echo '    "token": "insert-bot-token",' >> config.json
+echo '    "prefix": "?" ' >> config.json
+echo "}" >> config.json
+echo "# installing discord.js now with npm"
+echo npm install discord.js
+echo '#   Successfully created basic bot...'
+echo "# Please add your token in config.json"
+echo "# Deleted run.sh"
+echo rm ../run.sh
