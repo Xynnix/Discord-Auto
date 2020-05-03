bcd345d7324de4f7f26e091c2b6bb3939ad5677b
Xynnix <zkjcksn@gmail.com>
2020-05-03 05:46:47 -0400

    7th Commit

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 mkdir Bot && cd Bot
+
 touch index.js && touch config.json
 echo "const Discord = require('discord.js');" >> index.js
 echo "const client = new Discord.Client()" >> index.js
 echo "const config = require('./config.json');" >> index.js
-echo "  " >> index.js
+echo "const prefix = config.prefix;" >> index.js
+echo " " >> index.js
 echo "client.on('ready', () =>{" >> index.js
 echo "    console.log('Ready!');" >> index.js
-echo "};" >> index.js
+echo "});" >> index.js
 echo "client.on('message', (message) =>{" >>index.js
 echo "    if (message.author.bot) return;" >> index.js
 echo "    if (message.content === prefix + 'ping')" >> index.js
 echo "       message.channel.send('pang');" >> index.js
-echo "};" >> index.js
+echo "});" >> index.js
 echo "client.login(config.token);" >> index.js
 echo "{" >> config.json
 echo '    "token": "insert-bot-token",' >> config.json
