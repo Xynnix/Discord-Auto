638fd6b60930e571b3330c3e4734ed42bf9e6cab
NightRaven <53287573+Xynnix@users.noreply.github.com>
2020-10-29 00:44:23 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,26 @@
-mkdir Bot && cd Bot
+#!/bin/bash
 
+if [ -d "./Bot" ]
+then echo "Directory ./Bot exists"
+        echo "Overwrite? [ y / n]"
+        read yesno
+        if [ $yesno = "y" ]
+        then rm -r ./Bot
+        echo "Please Run this again to redo the setup!"
+        else echo "Exiting..."
+        fi
+else
+echo "Error: Directory /path/to/dir does not exist. Creating it now..."
+mkdir Bot && cd Bot
 touch index.js && touch config.json
+
+echo "Add your Token now..."
+read Token
+echo "Add your OwnerID now..."
+read OwnerID
+echo "Add your Prefix now."
+read Prefix
+
 echo "const Discord = require('discord.js');" >> index.js
 echo "const client = new Discord.Client()" >> index.js
 echo "const config = require('./config.json');" >> index.js
@@ -16,13 +36,22 @@ echo "       message.channel.send('pang');" >> index.js
 echo "});" >> index.js
 echo "client.login(config.token);" >> index.js
 echo "{" >> config.json
-echo '    "token": "insert-bot-token",' >> config.json
-echo '    "prefix": "?" ' >> config.json
+echo '    "token": "'$Token'",' >> config.json
+echo '    "OwnerID": "'$OwnerID'",' >> config.json
+echo '    "prefix": "'$Prefix'"' >> config.json
 echo "}" >> config.json
 echo "# installing discord.js now with npm"
 mv ../package.json .
 npm install
 echo '#   Successfully created basic bot...'
-echo "# Please add your token in config.json"
-echo "# Deleted run.sh"
-rm ../run.sh
+echo 'Delete run.sh? [ y / n ] [This is irreversable and will not allow you to redo this process!]'
+        read DeleteRun
+        if [ $DeleteRun = "y" ]
+        then rm -r ../run.sh
+        echo "# Deleted run.sh"
+        else
+        echo "Keeping run.sh!"
+        echo "Starting Bot..."
+        node ./index.js
+        fi
+fi
